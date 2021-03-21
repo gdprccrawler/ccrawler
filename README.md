8fb99e4d77ebbae626c830977b00f2b7a60f2f03
Filip Nyquist <fillerix99@gmail.com>
2021-03-21 15:41:26 +0100

    Added functions to find both accept and settings button

--- README.md
+++ README.md
@@ -10,6 +10,6 @@ A GPDR/Cookie consent crawler made for an exam project.
 
 ## Setup
 First create a virtual enviroment for this project.
-Run `python -m venv .venv` for windows or 'python3 -m venv .venv' for linux.
+Run `python -m venv .venv` for windows or `python3 -m venv .venv` for linux.
 
 More information to come....
--- main.py
+++ main.py
@@ -2,9 +2,21 @@
 import json
 import sys
 import time
+import os
+import string
+import random
 from loguru import logger as log
 from selenium import webdriver as wd
 from splinter import Browser as Sbrowser
+
+
+def genRunId(url):
+    """Takes in url, and turns it into a run ID for usage in the program."""
+    url = url.replace("https://", "").replace("http://", "").split(".")[0]
+    id = "".join(
+        random.choice(string.ascii_uppercase + string.digits) for _ in range(6)
+    )
+    return url + "-" + id
 
 
 def setupLogging():
@@ -41,13 +53,104 @@ def setupDriver(hless=False):
     return Sbrowser("chrome", options=browserOptions)
 
 
+def openDropdowns(browser):
+    """A function to open all dropdowns on a page, used before taking screenshot.
+
+    Args:
+        browser (WebDriver): The webdriver browser to use.
+    """
+    # Use js to open all dropdowns
+    browser.execute_script(
+        "document.querySelectorAll('.chevron').forEach(e => e.click())"
+    )
+
+
+def findByButtonString(browser, text):  # Return first element found
+    foundElements = browser.find_by_xpath(
+        f"//button[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'{text}')]"
+    )
+    if foundElements:
+        return foundElements.first
+    else:
+        return None
+
+
+def findByInputString(browser, text):  # Return first element found
+    foundElements = browser.find_by_xpath(f"//input[contains(@value,'{text}')]")
+    if foundElements:
+        return foundElements.first
+    else:
+        return None
+
+
+def checkCookieIframe(browser):  # Function to check if there is a iframe
+    foundIframes = browser.find_by_tag("iframe")  # onsent used for C/consent.
+    if foundIframes:
+        foundIframe = foundIframes.find_by_xpath("//*[contains(@title, 'onsent')]")
+        if foundIframe:
+            log.debug("Jumping into a found iframe...")
+            log.debug("Iframe: {}", foundIframes.first["id"])
+            browser.driver.switch_to.frame(foundIframes.first["id"])
+
+
+def findApproveButton(browser):
+    """Function that tries to find approve button on current page.
+
+    Args:
+        browser ([type]): The browser driver.
+
+    Returns:
+        [type]: Element found, else none.
+    """
+    log.debug("Looking for approve button.")
+    strings = ["pprove", "ccept", "gree", "lose", "odkänner", "örstår", "äng"]
+    for s in strings:
+        # Look for buttons with that string.
+        acceptButton = findByButtonString(browser, s)
+        if acceptButton is None:  # Only check for input if no button is found.
+            acceptButton = findByInputString(browser, s)
+        if acceptButton:
+            log.debug("Found an approve button on string {}, returning element.", s)
+            return acceptButton
+    log.debug("Did not find an approve button on page.")
+    return None
+
+
+def findPreferenceButton(browser):
+    """Function that tries to find settings button on current page.
+
+    Args:
+        browser ([type]): The browser driver.
+
+    Returns:
+        [type]: Element found, else none.
+    """
+    log.debug("Looking for settings button.")
+    strings = ["configure", "settings", "manage", "inställningar"]
+    for s in strings:
+        linksToManage = findByButtonString(browser, s)  # Look by button name.
+        if linksToManage is None:
+            linksToManage = browser.links.find_by_partial_text(s)  # Look by hefs.
+        if linksToManage:
+            log.debug("Found an settings button on string {}, returing element", s)
+            return linksToManage
+    log.debug("Did not find an settings button on page.")
+    return None
+
+
 def findCookieNotice(browser):
     """A function that searched for a GDPR/Cookie consent notice.
 
     Args:
         browser ([type]): [description]
     """
-    # Todo stuff here with browser.
+    path = os.path.abspath(os.getcwd())
+    gid = genRunId(browser.url)
+    checkCookieIframe(browser)
+    findApproveButton(browser)
+    findPreferenceButton(browser)
+    # Look for accept all button.
+    # Make nicer looking finder.
 
 
 @log.catch  # Lets be sure to catch all exceptions!
@@ -71,6 +174,8 @@ def startCrawl(url, browser):
     # Save down dict
     # Find cookie notice.
     findCookieNotice(browser)
+    time.sleep(2)
+    browser.quit()
 
 
 if __name__ == "__main__":
@@ -78,6 +183,8 @@ if __name__ == "__main__":
     setupLogging()
     browser = setupDriver(False)
     # Fetch the urls to be crawled.
-    urls = ["https://yahoo.com", "https://svt.se"]
+    urls = [
+        "https://svt.se",
+    ]
     for url in urls:
         startCrawl(url, browser)
